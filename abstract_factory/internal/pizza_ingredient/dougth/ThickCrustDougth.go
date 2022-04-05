963e5d2da95326d32dfd75800cd88ea41509efa1
ac2393921 <ac2393921@gmail.com>
2022-04-05 23:39:13 +0900

    add: pizza ingredient's dougth

---
+++ abstract_factory/internal/pizza_ingredient/dougth/ThickCrustDougth.go
@@ -0,0 +1,7 @@
+package pizza_ingredient
+
+type ThickCrustDougth struct{}
+
+func (t *ThickCrustDougth) ToString() string {
+	return "Thick Crust Dought"
+}
---
+++ abstract_factory/internal/pizza_ingredient/dougth/ThinCrustDougth.go
@@ -0,0 +1,7 @@
+package pizza_ingredient
+
+type ThinCrustDough struct{}
+
+func (t *ThinCrustDough) ToString() string {
+	return "Thin Crust Dough"
+}
