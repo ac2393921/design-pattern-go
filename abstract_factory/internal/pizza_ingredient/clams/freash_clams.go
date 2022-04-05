d7a19e255312405b1555bd97db0ca9e7a14558c8
ac2393921 <ac2393921@gmail.com>
2022-04-05 23:56:59 +0900

    add: pizza ingredient's clams

---
+++ abstract_factory/internal/pizza_ingredient/clams/freash_clams.go
@@ -0,0 +1,7 @@
+package pizza_ingredient
+
+type FreshClams struct{}
+
+func (f *FreshClams) ToString() string {
+	return "Fresh Clams"
+}
---
+++ abstract_factory/internal/pizza_ingredient/clams/frozen_clams.go
@@ -0,0 +1,7 @@
+package pizza_ingredient
+
+type FrozenClams struct{}
+
+func (f *FrozenClams) ToString() string {
+	return "Frozen Clams"
+}
